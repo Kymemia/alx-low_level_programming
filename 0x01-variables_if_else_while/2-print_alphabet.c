7a82e0de2af80259adeaf2eae7fc6b21f95d2d7b
Kymemia <kimemiachristopherr@gmail.com>
2023-10-07 01:46:14 -0700

    Ttwo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char myChar = 'a';
+
+	for (myChar = 'a'; myChar <= 'z'; myChar++)
+	{
+		putchar(myChar);
+	}
+	return (0);
+}
+
