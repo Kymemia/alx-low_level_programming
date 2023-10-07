5df0b5ab240d9546f93fa91ccddad90c0626b5be
Kymemia <kimemiachristopherr@gmail.com>
2023-10-07 01:04:43 -0700

    cfIV

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, n);
 	if (n % 10 > 5)
 	{
-		printf(" and is greater than 5\n");
+		printf(" and is greater than 5\n", n, n 10 %);
 	}
 	else if (n % 10 == 0)
 	{
-		printf(" and is 0\n");
+		printf(" and is 0\n", n, n 10 %);
 	}
 	else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n", n, n 10 %);
 	}
 	return (0);
 }
