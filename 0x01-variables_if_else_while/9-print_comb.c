b438cefef46713d77fc567f9da8fc9d43126d86c
Kymemia <kimemiachristopherr@gmail.com>
2023-10-07 05:04:27 -0700

    lD

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int lD = 0;
+
+	for (lD = 0; lD < 10; lD++)
+	{
+		putchar(lD + '0');
+		if (lD < 10)
+		{
+			putchar(',');
+			purchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
