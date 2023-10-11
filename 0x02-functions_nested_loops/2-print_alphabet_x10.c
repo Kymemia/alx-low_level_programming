4ed4db83ba073fe379337985960091483c0cfa8f
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 01:57:56 +0300

    twoo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int freq;
+
 	for (freq = 0; freq < 10; freq++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter)
+		for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
