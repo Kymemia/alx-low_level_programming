1f4d79b18a59f77b900911842bb77bab31f60ea5
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 00:58:25 +0300

    mi

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check he code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	for (char = 'A'; char <= 'Z'; char++)
+	{
+		_putchar("%c", char);
+	}
+
+	putchar('\n');
+	return (0);
+}
