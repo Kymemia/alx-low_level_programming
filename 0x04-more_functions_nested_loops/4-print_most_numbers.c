3e652d0e7222eb8946a20c7aa2a44d9662e16a5f
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 02:49:52 +0300

    four

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers in a range
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 && num == 4)
+		{
+			continue;
+		}
+		_putchar(num % 10 + '0');
+		_putchar('\n');
+	}
