8ea902ec0f45eb5875b949387877983cc2627ee7
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 13:50:04 +0300

    documentationfix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * Function _isalpha - Checks for alphabetic char.
+ * _isalpha - Checks for alphabetic char.
  * parameter C - character to be checked
  * Return: 1 if char. Return 0 otherwise.
  */
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Function - prints every minute of the day
+ * void jack_bauer: character to print output
+ * return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	char jack_bauer;
+
+	for (jack_bauer = 00 : 00; jack_bauer <= 23:59; jack_bauer++)
+		_putchar(jack_bauer);
+}
