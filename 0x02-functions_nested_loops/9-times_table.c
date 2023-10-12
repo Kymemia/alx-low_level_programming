7d039dcc87c04bcb0ebc436ca0be6d56f9cacf75
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 15:14:33 +0300

    nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table of 9, form 0
+ *
+ * @times_table - times table of 9
+ */
+
+void times_table(void)
+{
+	int num;
+	while (num <= 10)
+	{
+		int product = 9 * num;
+		_putchar (product = '0');
+		_putchar ('\n');
+		num++;
+	}
+}
