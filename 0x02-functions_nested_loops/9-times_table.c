d0437aea48ea84599c477247ce8388479f112e5a
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 18:03:11 +0300

    juu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (numm = 0; numm <= 9; numm++)
+		for (numm = 1; numm <= 9; numm++)
 		{
 			product = num * numm;
 			if (numm > 0)
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (product < 10)
+			if (product < 9)
 			{
 				_putchar(' ');
 			}
