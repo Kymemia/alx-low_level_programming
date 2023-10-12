4cd6abb15ed12a66fda4131a46925cf2e437d281
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 15:58:11 +0300

    hbh

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,13 +25,16 @@ void times_table(void)
 			if (product < 10)
 			{
 				_putchar(' ');
-				_putchar(product + '0');
-		}
-			else
+			}
+			if (product >= 10)
 			{
 				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
 			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar(product % 10 + '0');
 		}
 		_putchar('\n');
 	}
