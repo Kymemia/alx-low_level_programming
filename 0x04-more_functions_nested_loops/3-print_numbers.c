a3a2c910b788b5996237b12a64937fe53bb6200e
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 02:21:50 +0300

    three

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers in a range
- * @rR: int to be printed
+ * print_numbers - outputs numbers in a range
  * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 	int rR;
+
 	for (rR >= 0; rR <= 9; rR++)
 		_putchar(rR);
+	_putchar('\n');
 }
