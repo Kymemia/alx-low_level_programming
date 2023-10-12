9fbe30cf372992c4ce97d9ae776dc3381bcc8488
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 18:45:03 +0300

    eleven

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
 
 /**
  * add - adds two integers
- *
+ * @shrek: int to be added
+ * @donkey: int to be added
  * Return: Always 0 (Success)
  */
 
 int add(int shrek, int donkey)
 {
 	int sum;
+
 	sum = shrek + donkey;
-	return sum;
+	return (sum);
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from n to 98
+ * @n: int to be added
+ * @nat: int to be input
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n, nat)
+{
+	int n;
+
+	scanf("%d", &n);
+	for (nat = nat + 1; nat <= 98; nat++)
+	{
+		scanf("%d, ", &nat);
+	}
+}
