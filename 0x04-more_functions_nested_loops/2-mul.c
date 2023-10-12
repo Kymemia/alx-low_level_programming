94b6e9931dbdc20efce4dbf8a7e4e85794409604
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 02:12:58 +0300

    two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @shrek: first integer to multiply
+ * @donkey: second integer to multiply
+ * Return: Always 0 (Success)
+ */
+
+int mul(int shrek, int donkey)
+{
+	return (shrek * donkey);
+}
