47357ac89f80b33b3bdde423b336d7f60748e3e5
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 02:42:07 +0300

    yu

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,13 @@
 
 /**
  * print_numbers - outputs numbers in a range
- *
+ * @rR: int to initialize range
  * Return: Always 0 (Success)
  */
 
-void print_numbers(void)
+void print_numbers(int rR)
 {
-	int rR;
-
 	for (rR = 0; rR <= 9; rR++)
-	{
 		_putchar(rR + '0');
-	}
 	_putchar('\n');
 }
