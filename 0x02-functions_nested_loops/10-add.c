7a73f723a2821e6dd78ba2575cac87b209485b64
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 18:15:25 +0300

    ten

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int shrek, int donkey)
+{
+	int sum;
+	sum = shrek + donkey;
+	return sum;
+}
