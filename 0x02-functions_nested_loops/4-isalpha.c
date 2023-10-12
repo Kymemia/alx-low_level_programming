26d297e0077151df5a0d508f1250353f1d0028f5
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 03:32:57 +0300

    k

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && a <= 'z' || c >= 'A' && c <= 'Z' || c == ' '))
+	if ((c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == ' '))
 		return (1);
 	else
 		return (0);
