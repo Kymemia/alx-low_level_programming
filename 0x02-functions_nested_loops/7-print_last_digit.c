180a97839808ba9f9a7f16fb8a55bc69c8fc8891
Kymemia <kimemiachristopherr@gmail.com>
2023-10-12 04:30:20 +0300

    seven

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Function print_last_digit - prints last digit of number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int)
+{
+	_putchar((int % 10) + '0');
+}
