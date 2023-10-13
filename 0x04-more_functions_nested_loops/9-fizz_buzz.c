b0b715af0d70ce7f90480b6cf8c393833e26d98b
Kymemia <kimemiachristopherr@gmail.com>
2023-10-14 01:30:42 +0300

    triangle

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 			else if (num % 3 == 0)
 			{
-				printf("Fizz ");
+				printf("Fizz");
 			}
 				else if (num % 5 == 0)
 				{
-					printf("Buzz ");
+					printf("Buzz");
 				}
 					else
 					{
-						printf("%d ", num);
+						printf("%d", num);
 					}
 	}
 	printf("\n");
