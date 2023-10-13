6f59adc6539c6f95a64c2e216b5522357870913e
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 15:29:25 +0300

    cinqooo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 10 times
- *
- * Return: Always void
+ * more_numbers - prints numbers
  */
 
 void more_numbers(void)
@@ -11,11 +9,15 @@ void more_numbers(void)
 	int num;
 	int freq;
 
-	for (freq = 0; freq < 10; freq++)
+	for (freq = 0; freq <= 9; freq++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num);
+			if (num > 9)
+			{
+			_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
