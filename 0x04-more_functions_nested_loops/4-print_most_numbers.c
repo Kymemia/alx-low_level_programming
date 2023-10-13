6914739808c87f02e27940f96c42a96c6be32c45
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 14:54:30 +0300

    iffix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,10 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 || num == 4)
+		if (num != 2 && num != 4)
 		{
-			continue;
+			_putchar(num + '0');
 		}
-		_putchar(num);
 	}
 	_putchar('\n');
 }
