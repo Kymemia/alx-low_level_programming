77283fd677beadb267e26e01b7a2b167afd72416
Kymemia <kimemiachristopherr@gmail.com>
2023-10-13 03:03:00 +0300

    ffffour

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,15 @@
 
 void print_most_numbers(void)
 {
-	int num;
+	int num = 0;
 
-	for (num = 0; num <= 9; num++)
+	while (num <= 9)
 	{
-		if (num == 2 || num == 4)
+		if (num != 2 && num != 4)
 		{
-			continue;
+			_putchar(num + '0');
 		}
-		_putchar(num % 10 + '0');
-		_putchar('\n');
+		num++;
 	}
+	_putchar('\n');
 }
