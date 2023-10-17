7034dd105c7291f56f60a7507e5c7147e9784b94
Kymemia <kimemiachristopherr@gmail.com>
2023-10-18 01:42:59 +0300

    four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	for (; *str != '\0'; str--)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
