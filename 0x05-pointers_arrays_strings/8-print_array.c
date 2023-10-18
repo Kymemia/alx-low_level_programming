ae87d8a627c9c1c75216fff8027c937aa0298269
Kymemia <kimemiachristopherr@gmail.com>
2023-10-18 12:02:18 +0300

    eiti

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,13 @@ void print_array(int *a, int n)
 {
 	int rr;
 
-	for (int rr = 0; rr < n; rr++)
+	for (rr = 0; rr < n; rr++)
 	{
 		if (rr < n - 1)
 		{
 			_putchar(a[rr] + '0');
-			_putchar(', ');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
