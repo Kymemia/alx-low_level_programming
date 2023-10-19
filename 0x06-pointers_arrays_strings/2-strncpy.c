5546ac767073a51faac2e2d4abd819e679ba07f8
Kymemia <kimemiachristopherr@gmail.com>
2023-10-19 19:00:56 +0300

    cpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,17 +5,21 @@
  * @dest: string to be copied
  * @src: string to be copied
  * @n: string to be copied
+ * Return: copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int ww = 0;
 
-	while (src[ww] != '\0')
+	while (src[ww] != '\0' && ww < n)
 	{
 		dest[ww] = src[ww];
 		ww++;
 	}
-	dest[ww] = '\0';
-	dest[ww + n] = '\0';
+	while (ww < n)
+	{
+		dest[ww] = '\0';
+		ww++;
+	}
 	return (dest);
 }
