beea2f8448d43aa2159d1c3b09485546101264f9
Kymemia <kimemiachristopherr@gmail.com>
2023-10-20 01:50:21 +0300

    sixos

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,18 +10,18 @@ char *cap_string(char *str)
 	char *fangio = str;
 	int v = 0;
 	
-	if (str != 0 && *str >= 'a' && *str <= 'z')
+	if (str != 0 && str[v] >= 'a' && str[v] <= 'z')
 	{
-		*str = toupper(*str);
+		str[v] = toupper(str[v]);
 		v++;
 	}
 	while (str[v] != '\0')
 	{
 		if ((str[v] >= 'a' && str[v] <= 'z') && (str[v - 1] == ' ' || str[v - 1] == ',' || str[v - 1] == ';' || str[v - 1] == '.' || str[v - 1] == '!' || str[v - 1] == '?' || str[v - 1] == '"' || str[v - 1] == '(' || str[v - 1] ==  '\n' || str[v - 1] == ')' || str[v - 1] == '{' || str[v - 1] == '}' || str[v - 1] == '\t'))
 		{
-			*str = toupper(str[v]);
-			v++;
+			str[v] = toupper(str[v]);
 		}
+		v++;
 	}
 	return (fangio);
 }
