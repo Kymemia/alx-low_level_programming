1c5abc43e7006a5d36f50d333cfbfa3a0821749a
Kymemia <kimemiachristopherr@gmail.com>
2023-10-20 11:53:50 +0300

    ninetis

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _strcpy - function to copy string
+ * @dest: string to be copied
+ * @src: string to be copied
+ * Return: copied string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int destH = 0;
 	int ww = 0;
+	int ploombe;
 
 	while (dest[destH] != '\0')
 	{
@@ -16,6 +21,11 @@ char *_strcpy(char *dest, char *src)
 		dest[destH + ww] = src[ww];
 		ww++;
 	}
+	for (ploombe = 0; src[ploombe] != '\0'; ploombe++)
+	{
+		dest[ploombe] = src[ploombe];
+	}
 	dest[destH + ww] = '\0';
+	dest[ploombe] = '\0';
 	return (dest);
 }
