5f2db590d9003be5386d19e495e01f40a48caa4b
Kymemia <kimemiachristopherr@gmail.com>
2023-10-20 11:40:16 +0300

    nine

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int destH = 0;
+	int ww = 0;
+
+	while (dest[destH] != '\0')
+	{
+		destH++;
+	}
+	while (src[ww] != '\0')
+	{
+		dest[destH + ww] = src[ww];
+		ww++;
+	}
+	dest[destH + ww] = '\0';
+	return (dest);
+}
