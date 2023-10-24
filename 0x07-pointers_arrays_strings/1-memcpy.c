9292676512a0b84738237875c35f1c6e87f051cd
Kymemia <kimemiachristopherr@gmail.com>
2023-10-24 06:50:23 -0700

    dos

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy - copies memory area
+ * @dest: character to be copied
+ * @src: character to be copied
+ * @n: integer to be checked
+ * Return: Returns copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int ww;
+
+	do {
+		dest[ww] = src[ww];
+		ww++;
+	} while(ww < n);
+	return (dest);
+}
