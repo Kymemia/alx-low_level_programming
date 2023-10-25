f97f1b75d1f76cd244d25985b5afbc37f2c6f420
Kymemia <kimemiachristopherr@gmail.com>
2023-10-25 12:26:19 -0700

    crofty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-	for (; *s != '\0'; s++)
+	if (*s == '\0')
 	{
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
