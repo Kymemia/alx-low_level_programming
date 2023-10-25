83dbb8d68aa141e1372c3c0b1caa788a997f0c04
Kymemia <kimemiachristopherr@gmail.com>
2023-10-25 13:05:58 -0700

    Ltwo

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints string length
+ * @s: string to be counted
+ * Return: accurate string amount
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
