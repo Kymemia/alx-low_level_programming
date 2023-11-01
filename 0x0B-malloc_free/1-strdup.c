057bdf163aa847622807698f392938ea2784fb4c
Kymemia <kimemiachristopherr@gmail.com>
2023-10-31 23:13:10 -0700

    unooo

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - duplicates string
+ * @str: string to be duplicated
+ * Return: duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *clone = strdup(str);
+
+	if (clone == NULL)
+	{
+		return (NULL);
+	}
+	return (clone);
+}
