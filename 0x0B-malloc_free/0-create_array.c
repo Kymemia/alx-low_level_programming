b25bc15423403032e674d8ae12468b4f85b9d9b3
Kymemia <kimemiachristopherr@gmail.com>
2023-10-31 19:07:13 -0700

    zeerrooo

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates array of chars
+ * @size: int to be checked
+ * @c: char to be checked
+ * Return: Null if size is 0,
+ * else return array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	array = (char *)malloc(size);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
