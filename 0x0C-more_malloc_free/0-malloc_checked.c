06474bfc9fb260e203d298371ef28a4a2d9e0eba
Kymemia <kimemiachristopherr@gmail.com>
2023-11-02 16:38:52 -0700

    wann

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * malloc_checked - allocates memory
+ * @b: int to be checked
+ * Return: 98 if malloc fails,
+ * else return allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *prr;
-	
+
 	prr = malloc(10 * sizeof(b));
 	if (prr == NULL)
 	{
 		exit(98);
 	}
-	return(prr);
+	return (prr);
 }
