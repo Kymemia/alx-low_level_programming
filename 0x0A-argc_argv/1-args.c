91d6f0c24c54fcdf6a01e4849c2e0d911b02eda7
Kymemia <kimemiachristopherr@gmail.com>
2023-11-08 22:59:16 -0800

    bettyfix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - checks the program
+ * @argc: int to be checked
+ * @argv: char to be checked
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	(void) argv;
