14efe9968514719d8c22d6aaed91b907c954b3d9
Kymemia <kimemiachristopherr@gmail.com>
2023-11-11 21:37:34 -0800

    prep

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) abs((x) < 0 ? -(x) : (x))
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
