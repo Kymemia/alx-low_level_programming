7764cc32ffd2940821f956110fd40567f1a6502d
Kymemia <kimemiachristopherr@gmail.com>
2023-11-11 20:58:14 -0800

    opc

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,30 +1,44 @@
-#include <udis86.h>
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
+ * main - entry point of the program
+ * @argc: int to be checked
+ * @argv: char to be checked
+ * Return: error if number isn't correct,
+ * 2 if number of bytes is negative,
+ * 0 if code is successful
  */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int yy = 0;
-	int val = argc == 2? atoi(argv[1]) : 0;
+	int bytes, i;
+	int (*address)(int, char**) = main;
+	unsigned char opcode;
 
-	if (argc == 2 && val >= 0)
+	if (argc != 2)
 	{
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		for (yy = 0; ud_disassemble(&ud_obj); i++)
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
-	else if (val < 0)
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	for (i = 0; i < bytes; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i < bytes - 1)
+		{
+			printf(" ");
+		}
+
+		address++;
+	}
+	printf("\n");
+
 	return (0);
 }
