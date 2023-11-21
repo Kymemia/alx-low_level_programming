b467fafda7bf8c680c80401c0b67a9ecf16b49b6
Kymemia <kimemiachristopherr@gmail.com>
2023-11-21 14:47:54 +0300

    adds new node @ the beginning

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,10 +13,13 @@
 typedef struct listint_s
 {
 	int n;
+	char *str;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
