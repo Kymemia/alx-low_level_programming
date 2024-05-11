9028cfb47e142f6defa7bda75877a2864c33c694
Kymemia <kimemiachristopherr@gmail.com>
2024-05-12 02:05:54 +0300

    prototype fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int le_binary_search(int *array, size_t left, size_t right, int value);
 
 #endif
