aacc8f1de2ed39e92f500be2da00603d6454f945
Kymemia <kimemiachristopherr@gmail.com>
2024-05-12 02:18:16 +0300

    exponential take II

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -56,8 +56,6 @@ int exponential_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
-	if (array[0] == value)
-		return (0);
 
 	if (array[0] == value)
 		return (0);
